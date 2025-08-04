8d9368c692788efe85bde815960976fc3694fe0f
Marta Quiroga <martaqui133@gmail.com>
2025-08-04 19:32:28 +0200

    Configure users-permissions plugin with allowedFields for custom registration fields

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,34 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  "users-permissions": {
+    config: {
+      jwt: {
+        expiresIn: "30d",
+      },
+      ratelimit: {
+        interval: 60000,
+        max: 10,
+      },
+      user: {
+        privateAttributes: [
+          "password",
+          "resetPasswordToken",
+          "confirmationToken",
+          "email",
+        ],
+      },
+      register: {
+        enabled: true,
+        defaultRole: "authenticated",
+        allowedFields: [
+          "username",
+          "email",
+          "password",
+          "Nombre",
+          "primerApellido",
+          "segundoApellido",
+          "telefono",
+        ],
+      },
+    },
+  },
+});
