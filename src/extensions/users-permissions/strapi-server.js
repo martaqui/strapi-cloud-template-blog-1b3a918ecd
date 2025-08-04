7dffe78ad9f9691fbb019b3105fb82664770ead8
Marta Quiroga <martaqui133@gmail.com>
2025-08-04 19:06:02 +0200

    Simplify user registration: preserve original behavior and only add username generation

--- src/extensions/users-permissions/strapi-server.js
+++ src/extensions/users-permissions/strapi-server.js
@@ -24,73 +24,17 @@ module.exports = (plugin) => {
     ctx.body = sanitizeOutput(user);
   };
 
-  const sanitizeUser = (user) => {
-    const {
-      password,
-      resetPasswordToken,
-      confirmationToken,
-      ...sanitizedUser
-    } = user;
-    return sanitizedUser;
-  };
+  // Guardar el método register original
+  const originalRegister = plugin.controllers.auth.register;
 
   plugin.controllers.auth.register = async (ctx) => {
-    const pluginStore = await strapi.store({
-      type: "plugin",
-      name: "users-permissions",
-    });
-    const settings = await pluginStore.get({ key: "advanced" });
-
-    if (!settings.allow_register) {
-      return ctx.badRequest("Register action is currently disabled");
+    // Asegurar que username existe
+    if (!ctx.request.body.username) {
+      ctx.request.body.username = ctx.request.body.email.split("@")[0];
     }
 
-    const params = {
-      ...ctx.request.body,
-      provider: "local",
-    };
-
-    // Generar username si no se proporciona
-    if (!params.username) {
-      params.username = params.email.split("@")[0];
-    }
-
-    try {
-      const user = await strapi.service("plugin::users-permissions.user").add({
-        ...params,
-        role: settings.default_role,
-        email: params.email.toLowerCase(),
-        username: params.username,
-        confirmed: true,
-      });
-
-      const sanitizedUser = sanitizeUser(user);
-
-      if (settings.email_confirmation) {
-        try {
-          await strapi
-            .plugin("users-permissions")
-            .service("user")
-            .sendConfirmationEmail(sanitizedUser);
-        } catch (err) {
-          return ctx.badRequest(err.message);
-        }
-
-        return ctx.send({ user: sanitizedUser });
-      }
-
-      const jwt = strapi
-        .plugin("users-permissions")
-        .service("jwt")
-        .issue({ id: user.id });
-
-      return ctx.send({
-        jwt,
-        user: sanitizedUser,
-      });
-    } catch (err) {
-      return ctx.badRequest(err.message);
-    }
+    // Llamar al método original
+    return await originalRegister(ctx);
   };
 
   return plugin;
