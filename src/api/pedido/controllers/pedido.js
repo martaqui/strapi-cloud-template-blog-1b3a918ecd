e34780ea5794f1fbf85acb4e6ac4242a89d3e7ca
Marta Quiroga <martaqui133@gmail.com>
2025-08-08 14:23:49 +0200

    feat: auto-assign user to pedido on creation

--- src/api/pedido/controllers/pedido.js
+++ src/api/pedido/controllers/pedido.js
@@ -6,4 +6,21 @@
 
 const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController("api::pedido.pedido");
+module.exports = createCoreController("api::pedido.pedido", ({ strapi }) => ({
+  async create(ctx) {
+    // Obtener el usuario autenticado
+    const user = ctx.state.user;
+
+    if (!user) {
+      return ctx.unauthorized("Usuario no autenticado");
+    }
+
+    // Asignar el usuario al pedido
+    ctx.request.body.data.user = user.id;
+
+    // Llamar al método create original
+    const response = await super.create(ctx);
+
+    return response;
+  },
+}));
